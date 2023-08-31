b60815cea6c9da68f4e2a014ea3629d324a34c0a
Zombie <73972020+Shiro4k@users.noreply.github.com>
2023-08-31 13:18:34 +0800

    add Examples005 Condision if else

---
+++ Examples005_Condision_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Привет Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "коля")
+{
+    System.Console.WriteLine("Ура это же КОЛЯ");
+}
+else
+{
+    System.Console.WriteLine("Привет, ");
+    System.Console.WriteLine(username);
+}
